598d36c3b1d57f6b373291ccd1dfbc9a3af869d6
Jinal <jinal2002cllg@gmail.com>
2025-08-01 11:16:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+/* In the older vesion of JS the classes, arrow functions ,modules and other features were not available.And so the JS communities want to make sure the older version of the code continues to work.So using "use strict" allows JS to work as newer version.*/
+
+"use strict";  // this allow treat all JS code as newer version
+
+// alert(3+3)   // we are using nodejs, not browser so we can't use alert
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
